d32d7538ec6a9219f538e835c042038cf47cc51c
YesseniaWegiel94128 <YesseniaWegiel94128@salesperson.net>
2020-06-24 12:50:29 +0300

    1

---
+++ 1/src/index.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('gxojscocwj') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.clickid = "85495gxojscocwj42c";  tokens.uclick = "gxojscocwj"; 
